48979921a1358428495cafcfb5bd93916f6576ae
Amritha <amrithas172@gmail.com>
2024-03-18 19:59:10 +0530

    last commit

--- controller/adminController.js
+++ controller/adminController.js
@@ -6,7 +6,7 @@ const Order = require('../models/orderModel')
 
 const userList = async (req, res) => {
     const page = parseInt(req.query.page) || 1; 
-    const limit = 10;
+    const limit = 8;
     const skip = (page - 1) * limit; 
 
     try {
